1abea9f629b606808823bd703a0293f00a13b0cf
MM <x@y.com>
2024-09-15 19:43:39 +0000

    Add support for AIM experiment tracking

--- src/instructlab/training/main_ds.py
+++ src/instructlab/training/main_ds.py
@@ -21,7 +21,10 @@ from tqdm import tqdm
 from transformers import AutoModelForCausalLM, get_scheduler
 import deepspeed
 import torch
+# Imports for experiment tracking
 import wandb
+from aim import Run
+aimrun = None
 
 # First Party
 from instructlab.training import config
@@ -465,6 +468,28 @@ def train(args, model, tokenizer, train_loader, grad_accum, metric_logger):
                             "weight_norm": weight_norm,
                         }
                     )
+                global aimrun
+                if aimrun is not None:
+                    # Don't track gradnorm since it is None and AIM cannot handle that
+                    aimrun.track(
+                        {
+                            "epoch": epoch,
+                            "step": global_step,
+                            "rank": torch.distributed.get_rank(),
+                            "loss": loss.item(),
+                            "overall_throughput": overall_throughput,
+                            "lr": current_lr,
+                            "cuda_mem_allocated": cuda_mem_allocated,
+                            "cuda_malloc_retries": cuda_malloc_retries,
+                            "num_loss_counted_tokens": int(num_loss_counted_tokens),
+                            "batch_size": int(aggregated_values[1]),
+                            "total_loss": float(
+                                aggregated_values[2] / num_loss_counted_tokens
+                            ),
+                            "weight_norm": weight_norm,
+                        }
+                     )
+                  
 
             if args.save_samples > 0 and (
                 global_step * batch_size % args.save_samples == 0
@@ -764,6 +789,13 @@ if __name__ == "__main__":
         and int(os.getenv("LOCAL_RANK", "0")) == 0
     ):
         wandb.init(project=os.getenv("WANDB_PROJECT"))
+    # To enable AIM experiment tracking set the AIM_REMOTE_URL environment variable the remote URL e.g. aim://hostname:53800
+    if (
+        os.getenv("AIM_REMOTE_URL") is not None
+        and int(os.getenv("LOCAL_RANK", "0")) == 0
+    ):
+        aimrun = Run(repo=os.getenv("AIM_REMOTE_URL"))
+
     parser = argparse.ArgumentParser()
     parser.add_argument("--model_name_or_path", type=str)
     parser.add_argument("--data_path", type=str)
